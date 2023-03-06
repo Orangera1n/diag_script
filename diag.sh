520fd5f161e51bc6f096bf4b76e4e286ccb8df2a
Orangera1n <96156354+Orangera1n@users.noreply.github.com>
2023-03-06 08:07:34 -0500

    wow starting to make it work now

--- diag.sh
+++ diag.sh
@@ -1,9 +1,54 @@
-echo 
-set -e
-oscheck=$(uname)
-if [ ! -e "$oscheck"/gaster ]; then
-    curl -sLO https://nightly.link/verygenericname/gaster/workflows/makefile/main/gaster-"$oscheck".zip
-    unzip gaster-"$oscheck".zip
-    mv gaster "$oscheck"/
-    rm -rf gaster gaster-"$oscheck".zip
+echo "$1"
+echo "Welcome to @_orangera1n's diag_script"
+if [ "$1" = 'iPad4' ]; then
+    ./ipwnder -p &>/dev/null &
+    echo "Pwning device!"
+    ./ipwnder -f bootchian/iPad4/iBSS.img3 &>/dev/null &
+    echo "Sending iBSS!"
+    ./irecovery -f bootchain/ipad4/iBEC.img3 &>/dev/null &
+    echo "Sending iBEC!"
+    ./irecovery -f bootchain/ipad4/diag.img3 &>/dev/null &
+    echo "Sending Diags!"
+    ./irecovery -c go &>/dev/null &
+    echo "Loading Diags!"
+    echo "If the screen is purple now, you may use your DCSD cable to connect with MagicCFG, or use nanokdp to access the command line at usbserial-XXXXXX"
+    echo "You can use sn to write a new serial number, make sure its valid"
+elif [ "$1" = 'iPad2' ] || [  "$1" = 'iPad2,4' ] || [ "$1" = 'iPad3' ] || [ "$1" = 'iPadMini' ]; then
+    echo 'USE AN ARDUINO TO PUT YOUR DEVICE INTO PWNED DFU MODE'
+    ./ipwnder -f ./bootchain/"$1"/iBSS.img3
+    echo "Sending iBSS!"
+    ./irecovery -f ./bootchain/"$1"/iBEC.img3
+    echo "Sending iBEC!"
+    ./irecovery -f ./bootchain/"$1"/diag.img3
+    echo "Sending Diags!"
+    ./irecovery -c go
+    echo "Loading Diags!"
+    echo "If the screen is purple now, you may use your DCSD cable to connect with MagicCFG, or use nanokdp to access the command line at usbserial-XXXXXX"
+    echo "You can use sn to write a new serial number in the command line, make sure its valid"
+elif [ "$1" = 'iPhone6' ]||[ "$1" = 'iPhone6S' ]; then
+    echo "Pwning Device"
+    ./gaster pwn >/dev/null
+    ./gaster reset >/dev/null
+    echo "Sending iBSS"
+    ./irecovery -f bootchain/"$1"/iBSS.img4
+    echo "Sending iBEC"
+    ./irecovery -f bootchain/"$1"/iBEC.img4
+    echo "Sending Diags"
+    ./irecovery -f bootchain/"$1"/diag.img4
+    echo "Booting Diags"
+    ./irecovery -c go
+    echo "If the screen is a solid color now, you may use your DCSD cable to connect with MagicCFG, or use nanokdp to access the command line at usbserial-XXXXXX"
+elif [ "$1" = 'iPhoneX' ]; then
+    echo "Pwning Device"
+    ./gaster pwn >/dev/null
+    ./gaster reset >/dev/null
+    echo "Sending iBSS"
+    ./irecovery -f bootchain/"$1"/iBSS.img4
+    echo "Sending Diags"
+    ./irecovery -f bootchain/"$1"/diag.img4
+    echo "Booting Diags"
+    ./irecovery -c go
+    echo "If the screen is a solid color now, you may use your DCSD cable to connect with MagicCFG, or use nanokdp to access the command line at usbserial-XXXXXX"
+else
+    echo "Device Unsupported!" 1>&2
 fi
